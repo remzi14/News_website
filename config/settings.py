84b63535fa7240d2d86c7a2d0507b8ece57ba9c2
Ramizjon_14 <149583492+remzi14@users.noreply.github.com>
2025-02-09 19:38:45 +0500

    yana cod

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
-    "crispy_forms",
-    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
